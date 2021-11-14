1b59f9428e5014b3daa5bcd57a10fa8f0c26ba30
MinhThong <minhthongbkhn1994@gmail.com>
2021-11-14 19:08:29 +0700

    rewrite url react app

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(express.static('build'));
+app.use('*', function (request, response) {
+  response.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
